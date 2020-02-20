3972d785b3229f56f062c2ea6fcff25d33370368
ryuya226 <ryuya226@arsaga.jp>
2020-02-20 22:57:26 +0900

    Userモデルにvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :encrypted_password
+    validates :last_name
+    validates :first_name
+    validates :last_name_jp
+    validates :first_name_jp
+    validates :birth_year
+    validates :birth_month
+    validates :birth_day
+  end
 end
