aeec95da8f3cd7ba2a350eee1a5ab8b973f7dec9
ryuya226 <ryuya226@arsaga.jp>
2020-02-20 22:56:35 +0900

    usersテーブルのマイグレーションファイル作成

--- db/migrate/20200217122517_devise_create_users.rb
+++ db/migrate/20200217122517_devise_create_users.rb
@@ -3,22 +3,23 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
-      ## Recoverable
-      t.string   :reset_password_token
+      t.string :nickname,           null: false
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_jp,       null: false
+      t.string :first_name_jp,      null: false
+      t.integer :birth_year,        null: false
+      t.integer :birth_month,       null: false
+      t.integer :birth_day,         null: false
+      t.integer :phone_number
+      t.string :reset_password_token
       t.datetime :reset_password_sent_at
-
-      ## Rememberable
-      t.datetime :remember_created_at
-
-      
+      t.datetime :remember_created_at      
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
   end
 end
