857ce4b62446b576d18c4a154043f8050f4235f3
ryuya226 <ryuya226@arsaga.jp>
2020-02-21 19:34:38 +0900

    db:migrate

--- db/migrate/20200217122517_devise_create_users.rb
+++ db/migrate/20200217122517_devise_create_users.rb
@@ -3,9 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :nickname,           null: false
-      t.string :email,              null: false
-      t.string :encrypted_password, null: false
+      t.string :nickname,           null: false, comment: "ニックネーム"
+      t.string :email,              null: false, comment: "メールアドレス"
+      t.string :encrypted_password, null: false, comment: "パスワード(暗号化)"
       t.string :last_name,          null: false
       t.string :first_name,         null: false
       t.string :last_name_jp,       null: false
@@ -20,6 +20,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email,       unique: true
   end
 end
