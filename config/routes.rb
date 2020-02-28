5213907ad8d0ce9bb4acceb8f4fde3bdd8896cc4
sabawasabi <ON.0723_i@outlook.jp>
2020-02-28 23:53:52 +0900

    root_error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
   root "top#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-  root "products#new"
-  get 'products/new'
+  resources :products, only: [:new, :create]
 end
