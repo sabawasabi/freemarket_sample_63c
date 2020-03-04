09be5f6e4743ef223d87a9d091f5645ee46cb13a
sabawasabi <ON.0723_i@outlook.jp>
2020-03-04 17:49:56 +0900

    topから出品へ

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url,  'git@github.com:sabawasabi/freemarket_sample_63c.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, ‘2.5.1’
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/ASO_20200120.pem'] 
