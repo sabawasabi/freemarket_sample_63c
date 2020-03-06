d36b9f7c3a1151badb92a370c1a21ed4c5e7eea5
ryuya226 <ryuya226@arsaga.jp>
2020-03-06 18:56:35 +0900

    not null制約のテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,66 @@
 require 'rails_helper'
 describe User do
-  describe '#create' do
-    it "is invalid without a nickname" do
-      
+  describe '#createアクションのテスト' do
+    describe 'not nullのテスト' do
+      it "is invalid without a nickname" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it "is invalid without a email" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it "is invalid without a password" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it "is invalid without a last_name" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it "is invalid without a first_name" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it "is invalid without a last_name_jp" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it "is invalid without a first_name_jp" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it "is invalid without a birth_year" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it "is invalid without a birth_month" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it "is invalid without a birth_day" do
+        user = build(:user, nickname: "")
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
     end
   end
 end
