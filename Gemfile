77f73dfff79002b500749b0be6fda2d65b5d8aac
sabawasabi <ON.0723_i@outlook.jp>
2020-03-07 19:31:53 +0900

    商品出品機能
    外部キーの項目を除くカラムを登録出来るように設定。

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ gem 'carrierwave'
 gem 'mini_magick'
 
 gem 'fog-aws'
+
+gem 'pry-rails'
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,19 @@
 class ProductsController < ApplicationController
   def new
     @product = Product.new
-    @product.condition = "出品中"
   end
 
   def create
-    binding.pry
+    @product = Product.new(product_params)
+    if @product.save!
+      redirect_to root_path, notice: '商品を出品しました'
+    else
+      render :new
+    end
   end
 
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :condition, :shipping_charges, :shipping_area, :days_to_delivery, :price)
+  end
 end
