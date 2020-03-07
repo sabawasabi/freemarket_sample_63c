4bf8b2d166ee6278f644d063cefb5f136a43f3c6
sabawasabi <ON.0723_i@outlook.jp>
2020-03-07 16:51:56 +0900

    product_create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,11 @@
 class ProductsController < ApplicationController
   def new
+    @product = Product.new
+    @product.condition = "出品中"
   end
+
+  def create
+    binding.pry
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :condition, presence: true
+  validates :shipping_charges, presence: true
+  validates :shipping_area, presence: true
+  validates :days_to_delivery, presence: true
+  validates :price, presence: true
 end
