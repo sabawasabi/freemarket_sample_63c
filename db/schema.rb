ccaf48edf77d4b0192194ff48519833445a46454
sabawasabi <ON.0723_i@outlook.jp>
2020-03-09 22:04:38 +0900

    image_model_create
    商品出品に紐づける画像テーブルを作成中。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200303120845) do
+ActiveRecord::Schema.define(version: 20200309124849) do
+
+  create_table "product_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "image",      limit: 65535, null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                           null: false
