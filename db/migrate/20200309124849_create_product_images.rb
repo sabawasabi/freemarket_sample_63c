d94dc157ed75c7df8ea73ed256711099dd6f677b
sabawasabi <ON.0723_i@outlook.jp>
2020-03-10 22:45:23 +0900

    image_reset
    一度、image_productを削除。

--- db/migrate/20200309124849_create_product_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateProductImages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :product_images do |t|
-      # t.references :product_id, foreign_key: true
-      t.text :image, null: false
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200309124849) do
+ActiveRecord::Schema.define(version: 20200303120845) do
 
   create_table "product_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "image",      limit: 65535, null: false
