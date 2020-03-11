0ad411e7b7e642def527b12f40b0be9cda53e7d3
suminofu14 <red.chili.pepper14@gmail.com>
2020-03-11 22:51:26 +0900

    update_signin_function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  
   def new
     @product = Product.new
   end
@@ -19,5 +21,9 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:name, :description, :condition, :shipping_charges, :shipping_area, :days_to_delivery, :price)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "top#index"
   resources :products, only: [:new, :create, :show]
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
