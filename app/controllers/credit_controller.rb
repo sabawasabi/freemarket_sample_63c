7a5596d4661031f2aa0b75f44037a64c848a28f6
muneaki <muneaki89@gmail.com>
2020-03-11 21:23:55 +0900

    credit-markup

---
+++ app/controllers/credit_controller.rb
@@ -0,0 +1,4 @@
+class CreditController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "top#index"
   resources :products, only: [:new, :create]
+  resources :credit, only: [:new, :create]
 end
