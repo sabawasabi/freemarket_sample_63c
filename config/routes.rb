64e944ac3521ba01935d545ba80b7269ef32349a
sho560tanaka <58133463+sho560tanaka@users.noreply.github.com>
2020-03-14 17:22:52 +0900

    coment delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  root "users#show"
+
 # TODO usersのルーティング修正
-#   resources 'users', except: [:edit] do
-#     collection do
-#       get 'logout'
-#     end
-#   end
+  resources 'users', except: [:edit] do
+    collection do
+      get 'logout'
+    end
+  end
   
-  root "top#index"
+  
+  # root "top#index"
   resources :products, only: [:new, :create, :show]
 end
