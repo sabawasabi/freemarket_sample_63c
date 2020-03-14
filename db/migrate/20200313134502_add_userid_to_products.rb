ecf1784683244cd074371c9930ce67ffe8060660
suminofu14 <red.chili.pepper14@gmail.com>
2020-03-14 17:05:30 +0900

    delete_db_unnecessary_file

--- db/migrate/20200313134502_add_userid_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddUseridToProducts < ActiveRecord::Migration[5.0]
-  def change
-    add_column :products, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200313134502) do
+ActiveRecord::Schema.define(version: 20200314080154) do
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                           null: false
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20200313134502) do
     t.integer  "price",                          null: false
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
