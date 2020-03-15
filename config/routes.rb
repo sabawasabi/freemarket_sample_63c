3b7a0343a2dd55bc778d04b7475f5c564ac5660f
suminofu14 <red.chili.pepper14@gmail.com>
2020-03-15 14:03:21 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "top#index"
   resources :products, only: [:new, :create, :show]
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
   end
 end
