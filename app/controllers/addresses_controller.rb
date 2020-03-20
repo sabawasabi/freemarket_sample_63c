d9f3b733691341ad5e9020a8d9a37b8d9307b374
suminofu14 <red.chili.pepper14@gmail.com>
2020-03-20 20:28:08 +0900

    create_address_function

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,4 +2,14 @@ class AddressesController < ApplicationController
   def new
     @address = Address.new
   end
+
+  def create
+    Address.create(address_params)
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :prefectures, :city, :house_number, :building, :phone_number)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "top#index"
   resources :products, only: [:new, :create, :show]
-  resources :addresses, only: :new
+  resources :addresses, only: [:new, :create]
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
