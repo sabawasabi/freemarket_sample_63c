0a6d1abf2260010663fbb023d880545c29890524
suminofu14 <red.chili.pepper14@gmail.com>
2020-03-20 11:27:56 +0900

    create_form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "top#index"
   resources :products, only: [:new, :create, :show]
+  resources :addresses, only: :new
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
