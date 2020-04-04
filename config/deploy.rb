6efe4b8d39fa387c8df27cb4059f076722d1d0ba
sabawasabi <ON.0723_i@outlook.jp>
2020-04-04 12:12:31 +0900

    S3_test
    本番環境での画像アップロードテスト

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,14 +19,14 @@ set :keep_releases, 5
 
 set :linked_files, %w{ config/secrets.yml }
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
-  username: ENV["BASIC_AUTH_USER"],
-  password: ENV["BASIC_AUTH_PASSWORD"]
-}
+# set :default_env, {
+#   rbenv_root: "/usr/local/rbenv",
+#   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+#   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+#   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
+#   username: ENV["BASIC_AUTH_USER"],
+#   password: ENV["BASIC_AUTH_PASSWORD"]
+# }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
