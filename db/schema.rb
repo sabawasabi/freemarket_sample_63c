7cd6277443ded6715c84d3e4e61d58d54eb18c25
ryuya226 <ryuya226@arsaga.jp>
2020-04-05 17:51:44 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200330120928) do
+ActiveRecord::Schema.define(version: 20200324000001) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "last_name",               null: false
-    t.string   "first_name",              null: false
-    t.string   "last_name_jp",            null: false
-    t.string   "first_name_jp",           null: false
-    t.string   "postal_code",   limit: 7, null: false
-    t.string   "prefectures",             null: false
-    t.string   "city",                    null: false
-    t.string   "house_number",            null: false
+    t.integer  "postal_code",  null: false
+    t.string   "prefectures",  null: false
+    t.string   "city",         null: false
+    t.integer  "house_number", null: false
     t.string   "building"
     t.string   "phone_number"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
