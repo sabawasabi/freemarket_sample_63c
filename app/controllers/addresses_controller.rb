0509509528f9ca010354339f1c7be37a406306bf
suminofu14 <red.chili.pepper14@gmail.com>
2020-04-05 14:42:08 +0900

    fix_controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,6 @@
 class AddressesController < ApplicationController
+  before_action :set_address, only: [:edit, :update]
+  
   def new
     @address = Address.new
   end
@@ -13,13 +15,10 @@ class AddressesController < ApplicationController
   end
 
   def edit
-    @address = Address.find(params[:id])
   end
 
   def update
-    @address = Address.find(params[:id])
-    @address.update(address_params)
-    if @address.save
+    if @address.update(address_params)
       redirect_to edit_address_path(@address.id)
     else
       render :edit
@@ -29,6 +28,10 @@ class AddressesController < ApplicationController
   private
   def address_params
     params.require(:address).permit(:last_name, :first_name, :last_name_jp, :first_name_jp, :postal_code, :prefectures, :city, :house_number, :building, :phone_number)
+  end
+
+  def set_address
+    @address = Address.find(params[:id])
   end
 
 end
