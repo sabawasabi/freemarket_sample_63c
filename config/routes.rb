6de131dfa22cea48bd0ce85f5ed198f23f5385f5
suminofu14 <red.chili.pepper14@gmail.com>
2020-04-09 21:00:59 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,17 @@ Rails.application.routes.draw do
       get 'get_size', defaults: { format: 'json' }
     end
   end
-  resources :credits,   only: [:index, :new, :create]
+  resources :credits, only: [:new, :show, :destroy] do
+    collection do
+      post 'pay_show', to: 'credits#pay_show'
+      post 'pay', to: 'credits#pay'
+    end
+  end
+  resources :transactions, only: [:index] do
+    collection do
+      get 'pay_index', to: 'transactions#pay_index'
+      post 'pay', to: 'transactions#pay'
+      get 'done', to: 'transactions#done'
+    end
+  end
 end
