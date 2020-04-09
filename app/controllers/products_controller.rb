2026fc6ee7883eb4328510bac3b37a253b164162
suminofu14 <red.chili.pepper14@gmail.com>
2020-04-09 23:29:55 +0900

    update_products_show_view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
   def get_category_children
     #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
     @category_children = Category.find("#{params[:parent_id]}").children
- end
+  end
 
   # 子カテゴリーが選択された後に動くアクション
   def get_category_grandchildren
