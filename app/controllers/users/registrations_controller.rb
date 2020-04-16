8a61ba530491707581b928a40da00afed93a29a0
ryuya226 <ryuya226@arsaga.jp>
2020-04-16 21:41:55 +0900

    fix：文言修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,27 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters
-
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(configure_permitted_parameters)
-    if @user.save
-    redirect_to root_path
-    else
-      render :new
-    end
+    super
   end
 
   def edit
   end
 
   def update
-  end
-
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_jp, :first_name_jp, :birth_date, :phone_number, :profile, :icon])
   end
 end
